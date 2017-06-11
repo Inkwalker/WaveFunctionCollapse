f7481d99738d36342947f786783097086ba378eb
Inkwalker <cockroachslidy@gmail.com>
2017-06-11 22:56:36 +0300

    mesh edge detection

---
+++ Assets/Scripts/EdgeDebuger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using WaveFunctionCollapse.MeshTools;
+
+public class EdgeDebuger : MonoBehaviour
+{
+    public MeshFilter meshFilter;
+    public Vector3 axis;
+
+    private void OnDrawGizmosSelected()
+    {
+        if (meshFilter == null) return;
+
+        Mesh mesh = meshFilter.sharedMesh;
+        Transform transform = meshFilter.transform;
+
+        var edges = EdgeDetector.FindMeshEdges(axis, mesh);
+
+        Gizmos.color = Color.red;
+        foreach (var edge in edges)
+        {
+            Vector3 worldV0 = transform.TransformPoint(edge.vertices[0]);
+            Vector3 worldV1 = transform.TransformPoint(edge.vertices[1]);
+
+            Gizmos.DrawLine(worldV0, worldV1);
+
+            Gizmos.DrawSphere(worldV0, 0.01f);
+            Gizmos.DrawSphere(worldV1, 0.01f);
+        }
+
+        //Debug.Log("Vertices found: " + vertices.Length);
+    }
+}
---
+++ Assets/Scripts/MeshTools/EdgeDetector.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace WaveFunctionCollapse.MeshTools
+{
+    public static class EdgeDetector
+    {
+        public static Edge[] FindMeshEdges(Vector3 axis, Mesh mesh)
+        {
+            var vertices = GetBoundryVertices(axis, mesh);
+            vertices = RemoveDubles(vertices);
+
+            Edge[] result = FindEdges(vertices).ToArray();
+
+            return result;
+        }
+
+        private static List<Vertex> GetBoundryVertices(Vector3 axis, Mesh mesh)
+        {
+            Vector3 meshSize = mesh.bounds.extents;
+
+            Vector3 axisCenter = Vector3.Project(mesh.bounds.center, axis);
+            Vector3 axisDistance = Vector3.Project(meshSize, axis);
+
+            if (Vector3.Dot(axisDistance, axis) < 0)
+            {
+                axisDistance = -axisDistance;
+            }
+
+            axisDistance += axisCenter;
+
+            Vector3[] vertices = mesh.vertices;
+            int[] triangles = mesh.triangles;
+            
+            List<Vertex> edge = new List<Vertex>();
+
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                Vector3 vertexDistance = Vector3.Project(vertices[i], axis);
+                if (vertexDistance == axisDistance)
+                {
+                    Vertex edgeVertex = new Vertex(vertices[i]);
+
+                    for (int j = 0; j < triangles.Length; j++)
+                    {
+                        if (triangles[j] == i)
+                        {
+                            int triangleIndex = j / 3;
+                            edgeVertex.triangles.Add(triangleIndex);
+                        }
+                    }
+
+                    edge.Add(edgeVertex);
+                }
+            }
+
+            return edge;
+        }
+
+        private static List<Vertex> RemoveDubles(List<Vertex> vertices)
+        {
+            var unprocessed = new List<Vertex>(vertices);
+            var result = new List<Vertex>();
+
+            while (unprocessed.Count > 0)
+            {
+                Vertex vertex = unprocessed[0];
+                unprocessed.RemoveAt(0);
+
+                var doubles = unprocessed.FindAll((v) => v.position == vertex.position);
+
+                for (int i = 0; i < doubles.Count; i++)
+                {
+                    vertex.triangles.UnionWith(doubles[i].triangles);
+                    unprocessed.Remove(doubles[i]);
+                }
+
+                result.Add(vertex);
+            }
+
+            return result;
+        }
+
+        private static List<Edge> FindEdges(List<Vertex> vertices)
+        {
+            var edges = new List<Edge>();
+
+            for (int i = 0; i < vertices.Count; i++)
+            {
+                for (int j = i + 1; j < vertices.Count; j++)
+                {
+                    HashSet<int> common = new HashSet<int>(vertices[i].triangles);
+                    common.IntersectWith(vertices[j].triangles);
+                    if (common.Count == 1)
+                    {
+                        var edge = new Edge(vertices[i].position, vertices[j].position, common.Count);
+                        edges.Add(edge);
+                    }
+                }
+            }
+
+            return edges;
+        }
+
+        private class Vertex
+        {
+            public Vector3 position;
+            public HashSet<int> triangles;
+
+            public Vertex(Vector3 pos)
+            {
+                position = pos;
+                triangles = new HashSet<int>();
+            }
+        }
+
+        public struct Edge
+        {
+            public Vector3[] vertices;
+            public int triangles;
+
+            public Edge(Vector3 v1, Vector3 v2, int triangles)
+            {
+                vertices = new Vector3[2];
+                vertices[0] = v1;
+                vertices[1] = v2;
+
+                this.triangles = triangles;
+            }
+        }
+    }
+}
