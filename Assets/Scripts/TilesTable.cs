9d73f825ca7acb85c6c2143a161ae2de779646e4
Inkwalker <cockroachslidy@gmail.com>
2017-06-16 02:16:53 +0300

    tiles table

---
+++ Assets/Scripts/TilesTable.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using WaveFunctionCollapse.MeshTools;
+
+namespace WaveFunctionCollapse
+{
+    public class TilesTable
+    {
+        Dictionary<int, List<Tile>> edgesHashtable;
+
+        public TilesTable()
+        {
+            edgesHashtable = new Dictionary<int, List<Tile>>();
+        }
+
+        public void Add(Tile tile)
+        {
+            foreach (var edge in tile.Edges)
+            {
+                RegisterEdge(edge, tile);
+            }
+        }
+
+        public void AddRange(IEnumerable<Tile> tiles)
+        {
+            foreach (var tile in tiles)
+            {
+                Add(tile);
+            }
+        }
+
+        public Tile[] Get(int edgeHash)
+        {
+            if (edgesHashtable.ContainsKey(edgeHash))
+            {
+                return edgesHashtable[edgeHash].ToArray();
+            }
+
+            return new Tile[0];
+        }
+
+        private void RegisterEdge(MeshEdge edge, Tile tile)
+        {
+            if (edge == null || edge.Empty) return;
+
+            int edgeHash = edge.GetHashCode();
+
+            if (edgesHashtable.ContainsKey(edgeHash) == false)
+            {
+                edgesHashtable[edgeHash] = new List<Tile>();
+            }
+
+            edgesHashtable[edgeHash].Add(tile);
+        }
+    }
+}
