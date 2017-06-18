54137e590c70e7950396be58ee146c2e1be98e58
Inkwalker <cockroachslidy@gmail.com>
2017-06-19 00:42:19 +0300

    wave class

---
+++ Assets/Scripts/Wave.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace WaveFunctionCollapse
+{
+    public class Wave
+    {
+        Dictionary<Tile, int> tilesLookup;
+        Tile[] tiles;
+        bool[,,,] field;                        //values are inverted to save time on initialization
+
+        public Wave(int sizeX, int sizeY, int sizeZ ,Tile[] tiles)
+        {
+            this.tiles = tiles;
+            tilesLookup = new Dictionary<Tile, int>();
+            int tileIndex = 0;
+            foreach (var tile in tiles)
+            {
+                tilesLookup[tile] = tileIndex;
+                tileIndex++;
+            }
+
+            field = new bool[sizeX, sizeY, sizeZ, tiles.Length];
+        }
+
+        public void SetTileState(int x, int y, int z, Tile tile, bool possible)
+        {
+            int tileIndex = tilesLookup[tile];
+
+            field[x, y, z, tileIndex] = !possible;
+        }
+
+        public int GetPossibleTilesCount(int x, int y, int z)
+        {
+            int result = 0;
+            int totalTiles = tiles.Length;
+            for (int i = 0; i < totalTiles; i++)
+            {
+                if (field[x, y, z, i] == false) result++;
+            }
+
+            return result;
+        }
+
+        public Tile[] GetPossibleTiles(int x, int y, int z)
+        {
+            List<Tile> result = new List<Tile>();
+
+            int totalTiles = tiles.Length;
+            for (int i = 0; i < totalTiles; i++)
+            {
+                if (field[x, y, z, i] == false)
+                {
+                    result.Add(tiles[i]);
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
